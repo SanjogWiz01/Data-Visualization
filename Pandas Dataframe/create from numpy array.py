b88615bc60532ad30072f78db2179f1560ff87f7
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-01-18 17:13:09 +0545

    this is important

---
+++ Pandas Dataframe/create from numpy array.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+df = pd.DataFrame(data, columns=['A', 'B', 'C'], index=['Row1', 'Row2', 'Row3'])
+print(df)
+print(data)
