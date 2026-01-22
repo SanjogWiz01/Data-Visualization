75812ce90ba5c96d62f4a8eadb128d464923c679
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-01-22 21:44:56 +0545

    visualization

---
+++ Pandas Operation/Data manipulation.py
@@ -0,0 +1,9 @@
+''' Manipulation:
+
+Add / assign: df['new_col'] = ... or df.assign(...)
+
+Rename: df.rename(columns={...})
+
+Drop: df.drop(columns=[...]), df.drop(index=[...])
+
+Sort: df.sort_values(by=..., ascending=...)'''
