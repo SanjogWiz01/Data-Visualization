ae7c10a014f54dcbb6d2332b967a48129eb34113
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-01-25 23:01:50 +0545

    aggreate functions

--- Pandas Operation/data_analysis.py
+++ Pandas Operation/data_analysis.py
@@ -8,3 +8,12 @@ print(team.first()) # Let's print the first entries in all the groups formed.
 # grouping by multiple columns
 grouping = df.groupby(['Team', 'Position'])
 print(grouping.first())
+
+# grouping using aggregate functions
+aggregated_data = df.groupby(['Team', 'Position']).agg(
+    total_salary=('Salary', 'sum'),
+    avg_salary=('Salary', 'mean'),
+    player_count=('Name', 'count')
+)
+
+print(aggregated_data)
