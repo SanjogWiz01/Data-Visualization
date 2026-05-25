6bd7083032c76e5c8fb9aad06856a50ebf9a9a7e
Sanjog Poudel <sanjogpoudel384@gmail.com>
2026-05-25 21:42:42 +0545

    Update index.py

--- Pandas Series/index.py
+++ Pandas Series/index.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = pd.Series([10, 20, 30, 40], index=['a', 'b', 'c', 'd'])
 
 # Accessing the index
-print("Original Index:\n", data)
+print("Original Index:\n", data).  
 
 # Modifying the index
 data.index = ['w', 'x', 'y', 'z'] 
